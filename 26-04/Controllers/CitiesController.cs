28b8f88e46ecafc07e93e3594eb6cd6b10310413
Karina-Abjima <karina.gwalani@abjima.com>
2023-04-26 18:45:10 +0530

    day 2 .net api

---
+++ 26-04/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _26_04.Controllers
+
+
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetResult()
+        {
+            return new JsonResult(
+        new List<object>
+            {   new {id=1,Name ="new york city"},
+                new{id=2,Name="Antwerp"}
+
+            }
+            );
+
+        }
+    }
+}
