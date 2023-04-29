be472271d4bff8577f430a651ae72611ba9857bb
Karina-Abjima <karina.gwalani@abjima.com>
2023-04-29 18:47:38 +0530

    29-04 .net api

--- 26-04/Controllers/PointOfInterestController.cs
+++ 26-04/Controllers/PointOfInterestController.cs
@@ -1,6 +1,17 @@
-﻿namespace _26_04.Controllers
+﻿using _26_04.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _26_04.Controllers
 {
-    public class PointOfInterestController
+    [Route("api/cities/{cityId}/pointofinterest")]
+    [ApiController]
+    public class PointOfInterestController:ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestForCreationDto>> GetPointsOfInterest(int cityId)
+        {
+            var city= CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id==cityId);
+        }
     }
 }
