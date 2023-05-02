e452f744e9e4caf455f5a92cfd85ba37002992f3
Karina-Abjima <karina.gwalani@abjima.com>
2023-05-02 13:53:26 +0530

    .net api updated code

--- 26-04/Controllers/PointOfInterestController.cs
+++ 26-04/Controllers/PointOfInterestController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace _26_04.Controllers
 {
-    [Route("api/cities/{cityId}/pointofinterest")]
+    [Microsoft.AspNetCore.Components.Route("api/cities/{cityId}/pointofinterest")]
     [ApiController]
     public class PointOfInterestController:ControllerBase
     {
@@ -12,6 +12,31 @@ namespace _26_04.Controllers
         public ActionResult<IEnumerable<PointOfInterestForCreationDto>> GetPointsOfInterest(int cityId)
         {
             var city= CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id==cityId);
+            if(city == null) { return NotFound(); }
+            return Ok(city);
+
+            //finding point of interesr
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+            if(pointOfInterestId==Null)//recheck
+            {
+                return BadRequest();
+            }
+            return OkObjectResult;
         }
+        [HttpPost]
+        public ActionResult<PointOfInterestForCreationDto>CreatePOintOfOnterest(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+
+        }
+        [HttpPut]
+        public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId,
+            PointOfInterestForForUpdateDto pointOfInterest)
     }
 }
--- 26-04/Models/PointOfInterestForCreationDto.cs
+++ 26-04/Models/PointOfInterestForCreationDto.cs
@@ -1,7 +1,10 @@
-﻿namespace _26_04.Models
-{
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _26_04.Models
+{ 
     public class PointOfInterestForCreationDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }=string.Empty;
         public string? Description { get; set; }
     }
